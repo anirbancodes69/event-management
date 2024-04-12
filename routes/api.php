44356db6be577ebc7263da5294049d98e2713cc3
Anirban <anirbancodes@gmail.com>
2024-04-13 03:15:50 +0530

    setup authentication using sanctum and protected routes also

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\AttendeeController;
-use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\AttendeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('login', [AuthController::class, 'login']);
+
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::apiResource('event', EventController::class);
 

d5f1ca0e8f203a0c21d74c0378d4cdbf2997cca7
Anirban <anirbancodes@gmail.com>
2024-04-15 02:45:25 +0530

    added throttling

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -23,6 +23,8 @@ class AttendeeController extends Controller
     {
         $this->middleware('auth:sanctum')->except('index', 'show', 'update');
         $this->authorizeResource(Attendee::class, 'attendee');
+        $this->middleware('throttle:api')->only(['store', 'destroy']);
+
     }
 
     public function index(Event $event)
@@ -38,7 +40,7 @@ class AttendeeController extends Controller
     public function store(Request $request, Event $event)
     {
         $attendee = $this->loadRelationships($event->attendees()->create([
-            'user_id' => 1
+            'user_id' => $request->user()->id
         ]), $this->relations);
 
         return new AttendeeResource($attendee);

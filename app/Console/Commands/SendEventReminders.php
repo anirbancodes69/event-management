1606245d665679cc855bc56c09803bf76bf76ebb
Anirban <anirbancodes@gmail.com>
2024-04-15 02:14:26 +0530

    event reminder notifcation implementation

---
+++ app/Console/Commands/SendEventReminders.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Notifications\EventReminderNotification;
+use Event;
+use Illuminate\Support\Str;
+use Illuminate\Console\Command;
+
+class SendEventReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-event-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Reminder: send emails to all the users for events that are starting soon';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $events = \App\Models\Event::with('attendees.user')
+            ->whereBetween('start_time', [now(), now()->addDay()])
+            ->get();
+
+        $eventsCount = $events->count();
+
+        $pluralLabel = Str::plural('event', $eventsCount);
+
+        $events->each(
+            fn($event) => $event->attendees->each(
+                fn($attendee) => $attendee->user->notify(
+                    new EventReminderNotification($event)
+                )
+            )
+        );
+
+        $this->info("Found {$eventsCount} {$pluralLabel}");
+    }
+}
---
+++ app/Notifications/EventReminderNotification.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Event;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class EventReminderNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(
+        public Event $event
+    ) {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line('You have an upcoming event in 24 hours!')
+            ->action('View Event', route('event.show', $this->event->id))
+            ->line("The event {$this->event->name} will start at {$this->event->start_time}");
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'event_id' => $this->event->id,
+            'event_name' => $this->event->event_name,
+            'start_time' => $this->event->start_time
+        ];
+    }
+}
